ae98eccc01e73221b58dbc1244175c34e452cfd3
Asmit Dash <115637270+asmitdash@users.noreply.github.com>
2023-12-26 23:37:01 +0530

    Add files via upload

---
+++ task4/js/scripts.js
@@ -0,0 +1,9 @@
+// Add your JavaScript code here
+
+// Example: Add a simple interactive element
+document.addEventListener("DOMContentLoaded", function () {
+    const skillsSection = document.getElementById("skills");
+    skillsSection.addEventListener("click", function () {
+        alert("You clicked the Skills section!");
+    });
+});
